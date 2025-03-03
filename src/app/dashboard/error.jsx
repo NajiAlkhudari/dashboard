e839907e37ba70ef198f15adf508625ca56e3abc
NajiAlkhudari <najikh98@gmail.com>
2025-03-03 17:23:03 +0300

    update error

--- src/app/dashboard/error.jsx
+++ src/app/dashboard/error.jsx
@@ -8,24 +8,9 @@ export default function Error({ error, reset }) {
   }, [error]);
 
   return (
-    <div
-    className="bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3 shadow-md"
-    role="alert"
-  >
-    <div className="flex">
-      <div className="py-1">
-        <svg
-          className="fill-current h-6 w-6 text-red-500 mr-4"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
-        </svg>
-      </div>
-      {error.message}
-      <div>
-        </div>
-        </div>
+    <div className="flex flex-col items-center justify-center h-screen bg-red-800">
+      <h2 className="text-xl font-semibold text-red-300">Something went wrong!</h2>
+      <p className="border border-spacing-1 border-black px-24 py-2 bg-gray-950 mt-2 text-2xl text-red-600 font-sans font-bold">{error.message}</p>
       <button
         onClick={() => reset()}
         className="mt-4 px-4 py-2 bg-white text-black rounded hover:bg-gray-300 "
